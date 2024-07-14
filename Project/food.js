3e70c16984ae4edf3a103910c44deb6882684ec0
SAMUVEL REEGAN P <164333238+SAMUVELREEGAN@users.noreply.github.com>
2024-07-14 12:22:31 +0530

    Add files via upload

---
+++ Project/food.js
@@ -0,0 +1,80 @@
+var c=0
+function a1(){
+  c=c+1
+  document.getElementById("p").innerHTML=c
+}
+function b1(){
+  c=c-1
+  document.getElementById("p").innerHTML=c
+}
+//  
+var d=0
+function a(){
+  d=d+1
+  document.getElementById("p1").innerHTML=d
+}
+function b(){
+  d=d-1
+  document.getElementById("p1").innerHTML=d
+}
+// 
+
+var e=0
+function a2(){
+  e=e+1
+  document.getElementById("p2").innerHTML=e
+}
+function b2(){
+  e=e-1
+  document.getElementById("p2").innerHTML=e
+}
+// 
+var f=0
+function a3(){
+  f=f+1
+  document.getElementById("p3").innerHTML=f
+}
+function b3(){
+  f=f-1
+  document.getElementById("p3").innerHTML=f
+}
+// 
+var g=0
+function a4(){
+  g=g+1
+  document.getElementById("p4").innerHTML=g
+}
+function b4(){
+  g=g-1
+  document.getElementById("p4").innerHTML=g
+}
+// 
+var h=0
+function a5(){
+  h=h+1
+  document.getElementById("p5").innerHTML=h
+}
+function b5(){
+  h=h-1
+  document.getElementById("p5").innerHTML=h
+}
+// 
+var i=0
+function a6(){
+  i=i+1
+  document.getElementById("p6").innerHTML=i
+}
+function b6(){
+  i=i-1
+  document.getElementById("p6").innerHTML=i
+}
+// 
+ var j=0
+function a7(){
+  j=j+1
+  document.getElementById("p7").innerHTML=j
+}
+function b7(){
+  j=j-1
+  document.getElementById("p7").innerHTML=j
+}
